2dc57250a7a37c257f4c182d3a3f7ea4d5772c54
Vedran Bojanic <Vedran.Bojanic@extern.consorsbank.de>
2018-02-20 21:31:14 +0100

    Connect and disconnect event emmiters added.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const path = require('path');
 const express = require('express');
-var app = express();
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 // Defined port for app to run on
 app.set('port', 3000);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+   console.log('New user connected!');
+
+   socket.on('disconnect', () => {
+      console.log('User disconnected from server.')
+   });
+});
+
 // Listen the requests
-app.listen(app.get('port'), () => {
+server.listen(app.get('port'), () => {
    console.log('Server is up on', app.get('port'));
 });
